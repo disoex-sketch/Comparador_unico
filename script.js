45cee9f822e1e297a7146887670a6c74dd62ca61
disoex-sketch <disoex@gmail.com>
2026-09-23 16:07:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { foto1: 'foto1_antes.jpg', foto2: 'foto1_despues.jpg' },
     { foto1: 'foto2_antes.jpg', foto2: 'foto2_despues.jpg' },
     { foto1: 'foto3_antes.jpg', foto2: 'foto3_despues.jpg' },
-    { foto1: 'foto4_antes.jpg', foto2: 'foto4_despues.jpg' },
+    { foto1: 'foto4_antes.jpg', foto2: 'foto4_despues.jpg' }
   ];
 
   let indiceActual = 0;
